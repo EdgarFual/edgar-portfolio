15ea9222b4f8314239653ea4daa4d82f16a21b8a
Edgar Lizardo Fuentes Alvarado <122401915+EdgarFual@users.noreply.github.com>
2025-09-02 10:49:43 -0500

    cambios 02092025
    hora 1049am

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -35,30 +35,31 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>Las skills que domino son tecnologías que desarrollado en mi experencia porfesional y laboral.<br></br> Java, PHP, CSS, Flutter, Dart, JS, HTML, Boostrap, Angular, Node, Tailwind, SQL Server, MySQL, Firebase, GitLab, GitHub.<br></br>Tableu, PowerBi, FoxPro, Excel Intermedio</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Desarrollar Web</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Desarrollador Móvil</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={meter3} alt="Image" />
+                                <h5>Ingeniero de Datos</h5>
                             </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>Ingeniero TI</h5>
+                            </div>
+                            
                         </Carousel>
                     </div>
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+        
     </section>
   )
 }
